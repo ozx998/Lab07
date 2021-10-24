014da9080884c34cf6a632c4591d1dc585cd0057
ozx998 <91934397+ozx998@users.noreply.github.com>
2021-10-24 23:55:54 +0800

    jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    public GameObject player;
+    private Rigidbody rgby;
+    public float jumpForce;
     void Start()
     {
+        rgby = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
 
     void Update()
     {
+        float yVelocity = 0;
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            yVelocity = jumpForce;
+            
+        }
+        rgby.velocity = new Vector3(rgby.velocity.x, rgby.velocity.y + yVelocity, rgby.velocity.z);
+
     }
 }
